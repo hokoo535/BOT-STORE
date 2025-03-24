5870b34dd5444e977b68be1842dbc78583904ae4
joyesbot <lelandeverhart9@gmail.com>
2025-03-24 19:20:24 -0400

    Update index.js

--- index.js
+++ index.js
@@ -37,8 +37,8 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["🎧 Listening to Spotify", "🎮 Playing VALORANT"];
-const statusTypes = [ 'dnd', 'idle'];
+const statusMessages = ["🛠️Created by, CoreX | Produtions", "🔗 https://linktr.ee/CoreX25"];
+const statusTypes = [ 'online'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
 
